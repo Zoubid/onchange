0ab5bebaf409efcacd6b3a3b6b4e7819956059a6
zoubid <empressbida@gmail.com>
2017-09-03 11:15:04 +0100

    [master]: fixed the onchnage

--- onchange_model.py
+++ onchange_model.py
@@ -2,14 +2,15 @@ from openerp import models, fields, api
 
 class theonchange(models.Model):
     _name = 'product.model'
-    amount = fields.Float('Buy Price')
-    unit_price=fields.Float('Shipping price')
-    price =fields.Float('standard price')
+    amount = fields.Float('Buy Price',0)
+    unit_price=fields.Float('Shipping price',0)
+    price =fields.Float('standard price',0)
 
 
+    # @api.multi
     @api.onchange('amount', 'unit_price')
-        def _onchange_price(self):
-            # set auto-changing field
-            self.price = self.amount * self.unit_price
-            # Can optionally return a warning and domains
-            return True
+    def _onchange_price(self):
+        # set auto-changing field
+        result = self.amount * self.unit_price
+        self.price =result
+        # Can optionally return a warning and domains
